3f72a614c6986d9b79daa4e437c2d27947347b51
Uli Kusterer <ulivc@zathras.de>
2015-08-09 00:06:52 +0200

    Initial Commit

---
+++ FileDisk/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FileDisk
+//
+//  Created by Uli Kusterer on 09/08/15.
+//  Copyright (c) 2015 Uli Kusterer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
